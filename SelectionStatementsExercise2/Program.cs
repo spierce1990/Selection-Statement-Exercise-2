bd30f3bbb42a5f1d6991adb1b0de8f12a429c29d
Sionylen <sionylenpierce@outlook.com>
2022-11-28 15:43:06 -0600

    submit now

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,30 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is your favorite subject?");
+            var userFavSub = (Console.ReadLine());
+
+            switch (userFavSub.ToLower())
+            {
+                case "history":
+                    Console.WriteLine("History Facts: There were female Gladiators in Ancient Rome!");
+                    break;
+                case "math":
+                    Console.WriteLine("Math Facts: Every odd number, when written in English, contains an “e”. ");
+                    break;
+                case "english":
+                    Console.WriteLine("English Literature Facts: John Milton when composed “Paradise lost” was blind.");
+                    break;
+                case "science":
+                    Console.WriteLine("Science Fact: A cockroach can live for up to one week without its head.");    
+                    break;
+                case "arts":
+                    Console.WriteLine("Facts about Arts: The Mona Lisa has her own mailbox in the Louvre because of all the love letters she receives");
+                    break;
+                default:
+                    Console.WriteLine("Invalid Choice");
+                    break;
+            }
         }
     }
 }
